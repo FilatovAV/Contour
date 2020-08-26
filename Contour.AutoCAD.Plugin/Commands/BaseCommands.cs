89853183e2b4fdb178455a417d1cdaf15d3471b6
FilatovAV <a777f@mail.ru>
2020-08-26 20:37:01 +0300

    Added a command 'ContourPalette' and changed cuix file

---
+++ Contour.AutoCAD.Plugin/Commands/BaseCommands.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GeobridgeClassLibrary;
+
+namespace Contour.AutoCAD.Plugin.Commands
+{
+    public class BaseCommands
+    {
+        [Autodesk.AutoCAD.Runtime.CommandMethod("ContourPalette", Autodesk.AutoCAD.Runtime.CommandFlags.NoHistory)]
+        public void MainPaletteShow()
+        {
+            try
+            {
+                Main.MainPaletteSet.Visible = !Main.MainPaletteSet.Visible;
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show($"Исключение в GetContour\n{ex.Message}", CommonMessages.GeobridgeContours, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
